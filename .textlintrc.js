083aa6af7b59b506a880e5421c60a0f7d92c3983
kimizuy <kimizuyamasaki@gmail.com>
2020-09-24 20:13:33 +0900

    change .js

---
+++ .textlintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  filters: {},
+  rules: {
+    'preset-ja-spacing': {
+      'ja-space-between-half-and-full-width': {
+        space: 'always'
+      }
+    },
+    'preset-ja-technical-writing': true,
+    'spellcheck-tech-word': true
+  }
+}
